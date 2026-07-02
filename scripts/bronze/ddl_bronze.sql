69300369e26c75aa80f44a0be6fb2598d96233f2
ykartik012007-sudo <ykartik012007@gmail.com>
2026-07-02 18:28:23 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,1 +1,94 @@
+/*
+=================================================================
+                  DDL Script Bronze Layer
+=================================================================
+Script Purpose : This Script Creating table into Bronze Schema
+                 And if table is already existing  it will drop it 
+                 and Recreate
+===================================================================
 
+*/
+---Creating table (bronze.crm_cust_info) into bronze schema from source (crm) 
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info
+(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname VARCHAR(50),
+cst_marital_status VARCHAR(50),
+cst_gndr VARCHAR(50),
+cst_create_date DATE
+);
+GO
+---Creating table (bronze.crm_prd_info) into bronze schema from source (crm)
+IF OBJECT_ID('bronze.crm_prd_info' , 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+
+CREATE TABLE bronze.crm_prd_info
+(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
+GO
+
+---Creating table (bronze.crm_sales_details) into bronze schema from source (crm)
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details ;
+
+CREATE TABLE bronze.crm_sales_details
+(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id	INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+);
+GO
+
+--Creating Table (bronze.erp_cust_az12) into bronze schema from source (erp)
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL 
+	DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12
+(
+cid NVARCHAR(50),
+bdate DATE,
+GEN NVARCHAR(50)
+);
+GO
+
+--Creating TABLE (bronze.erp_loc_a101) into bronze schema from source (erp)
+IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+
+CREATE TABLE bronze.erp_loc_a101
+(
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+);
+GO
+
+--Creating TABLE (bronze.erp_px_cat_g1v2) into bronze schema from source (erp)
+IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2
+(
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
+GO
